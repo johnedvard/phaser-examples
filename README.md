bdf328d05286a4c92bc3820fe80f78271f5bfb72
John Edvard Reiten <johnedvard89@gmail.com>
2023-07-10 21:23:39 +0900

    docs: add README

---
+++ README.md
@@ -0,0 +1,12 @@
+## Summary
+
+Phaser examples. Also live at https://codepen.io/ReitGames
+
+```
+npm install
+npm run dev
+```
+
+Open http://127.0.0.1:5174/ in your browser
+
+Go to `main.js` and change the imported example to run it.
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 import './style.css';
-import { createExample } from './public/examples/vector-vs-image';
+import { createExample } from './public/examples/vector-vs-image'; // change the path to the example you want to run
 
 createExample();
